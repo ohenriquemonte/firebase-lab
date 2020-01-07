1ddbda38041ec5670e0434a288b78cd68ecd7a17
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2020-01-07 20:31:22 -0300

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { FirebaseX } from '@ionic-native/firebase-x/ngx';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
 
+  constructor(private firebaseX: FirebaseX) {
+    this.firebaseX.getToken()
+      .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
+      .catch(error => console.error('Error getting token', error));
+
+    this.firebaseX.onMessageReceived()
+      .subscribe(data => console.log(`User opened a notification ${data}`));
+
+    this.firebaseX.onTokenRefresh()
+      .subscribe((token: string) => console.log(`Got a new token ${token}`));
+  }
 }
